7cb28a1942f95ae6e06c2296ad47490125e60e9b
RiverWind1024 <riverwind1024@users.noreply.github.com>
2026-08-23 22:50:52 +0800

    refactor(s3): S3SecretStore 抽象，解除 CLI 对 flutter_secure_storage 的依赖

--- lib/features/gallery/gallery_provider.dart
+++ lib/features/gallery/gallery_provider.dart
@@ -25,6 +25,7 @@ import '../../services/file_storage_service.dart';
 import '../../core/ocr/tesseract_bindings.dart';
 import '../../services/log_service.dart';
 import '../../services/s3_config.dart';
+import '../../services/s3_secret_store.dart';
 import '../../services/s3_sync_service.dart';
 import '../../services/s3_sync_serializer.dart';
 import '../../services/import_service.dart';
@@ -933,6 +934,7 @@ final s3SyncServiceProvider = Provider<S3SyncService>((ref) {
       albumRepo: ref.read(albumRepositoryProvider),
       db: db,
     ),
+    secretStore: const FlutterSecureStorageS3SecretStore(),
     db: db,
     log: ref.read(logServiceProvider),
   );
